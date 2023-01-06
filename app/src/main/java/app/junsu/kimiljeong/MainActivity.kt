705b4379d284965354e1f135583ba316ff535b49
JunJaBoy <krpjs0508@gmail.com>
2023-01-06 17:52:37 +0900

    Initial commit

---
+++ app/src/main/java/app/junsu/kimiljeong/MainActivity.kt
@@ -0,0 +1,11 @@
+package app.junsu.kimiljeong
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
