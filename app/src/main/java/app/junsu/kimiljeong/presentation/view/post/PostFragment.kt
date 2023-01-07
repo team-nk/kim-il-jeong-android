f87788acf0b2f6b3633e4b2568958dbe6f57684f
Tmdhoon2 <hks026naver@gmail.com>
2023-01-07 20:48:16 +0900

    feat :: PostFragment publishing

---
+++ app/src/main/java/app/junsu/kimiljeong/presentation/view/post/PostFragment.kt
@@ -0,0 +1,21 @@
+package app.junsu.kimiljeong.presentation.view.post
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import app.junsu.kimiljeong.R
+import app.junsu.kimiljeong.databinding.FragmentCalendarBinding
+import app.junsu.kimiljeong.databinding.FragmentMapBinding
+import app.junsu.kimiljeong.presentation.base.view.BaseFragment
+
+class PostFragment : BaseFragment<FragmentMapBinding>(
+    R.layout.fragment_calendar,
+) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
