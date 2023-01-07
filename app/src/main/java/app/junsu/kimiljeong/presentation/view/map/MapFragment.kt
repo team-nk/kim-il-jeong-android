70e2dcedac24cf5fe660a3a3ec551be81ef67a9c
Tmdhoon2 <hks026naver@gmail.com>
2023-01-07 19:43:35 +0900

    feat :: MapFragment publishing

---
+++ app/src/main/java/app/junsu/kimiljeong/presentation/view/map/MapFragment.kt
@@ -0,0 +1,21 @@
+package app.junsu.kimiljeong.presentation.view.map
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import app.junsu.kimiljeong.R
+import app.junsu.kimiljeong.databinding.FragmentCalendarBinding
+import app.junsu.kimiljeong.databinding.FragmentMapBinding
+import app.junsu.kimiljeong.presentation.base.view.BaseFragment
+
+class MapFragment : BaseFragment<FragmentMapBinding>(
+    R.layout.fragment_calendar,
+) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
