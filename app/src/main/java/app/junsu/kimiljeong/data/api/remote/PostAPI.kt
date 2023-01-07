7b3494bf5f0c91090d1316cf96d6177c78cc5080
JunJaBoy <krpjs0508@gmail.com>
2023-01-07 19:48:45 +0900

    feat :: Comment 관련 API 구현

--- app/src/main/java/app/junsu/kimiljeong/data/api/remote/PostAPI.kt
+++ app/src/main/java/app/junsu/kimiljeong/data/api/remote/PostAPI.kt
@@ -2,11 +2,13 @@ package app.junsu.kimiljeong.data.api.remote
 
 import app.junsu.kimiljeong.data.model.remote.request.CreatePostRequest
 import app.junsu.kimiljeong.data.model.remote.response.InquireBirthdayListResponse
+import app.junsu.kimiljeong.data.model.remote.response.InquireCommentListResponse
 import app.junsu.kimiljeong.data.model.remote.response.InquirePostListResponse
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface PostAPI {
 
@@ -20,4 +22,14 @@ interface PostAPI {
 
     @GET("/post/birthday")
     fun inquireBirthdayList(): Response<InquireBirthdayListResponse>
+
+    @GET("/comment/{post-id}")
+    fun inquireCommentList(
+        @Path("post-id") postId: Int,
+    ): Response<InquireCommentListResponse>
+
+    @POST("/comment/{post-id}")
+    fun createComment(
+        @Path("post-id") postId: Int,
+    ): Response<Void>
 }
