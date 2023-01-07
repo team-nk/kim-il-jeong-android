82b103517d13eca1d6f341d600fed4260089840a
JunJaBoy <krpjs0508@gmail.com>
2023-01-07 14:47:50 +0900

    feat :: UploadImageResponse 구현

---
+++ app/src/main/java/app/junsu/kimiljeong/data/model/remote/response/UploadImageResponse.kt
@@ -0,0 +1,7 @@
+package app.junsu.kimiljeong.data.model.remote.response
+
+import com.google.gson.annotations.SerializedName
+
+data class UploadImageResponse(
+    @SerializedName("image_url") val imageUrl: String,
+)
