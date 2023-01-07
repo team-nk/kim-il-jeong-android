c3f97b49a8971bc097d2e5dabbd9904e0eb9bd34
JunJaBoy <krpjs0508@gmail.com>
2023-01-07 18:09:22 +0900

    feat :: PostAPI 구현

--- app/src/main/java/app/junsu/kimiljeong/data/api/remote/PostAPI.kt
+++ app/src/main/java/app/junsu/kimiljeong/data/api/remote/PostAPI.kt
@@ -1,6 +1,23 @@
 package app.junsu.kimiljeong.data.api.remote
 
+import app.junsu.kimiljeong.data.model.remote.request.CreatePostRequest
+import app.junsu.kimiljeong.data.model.remote.response.InquireBirthdayListResponse
+import app.junsu.kimiljeong.data.model.remote.response.InquirePostListResponse
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+
 interface PostAPI {
 
-    @
+    @GET("/post")
+    fun inquirePostList(): Response<InquirePostListResponse>
+
+    @POST("/post")
+    fun createPost(
+        @Body createPostRequest: CreatePostRequest,
+    ): Response<Void>
+
+    @GET("/post/birthday")
+    fun inquireBirthdayList(): Response<InquireBirthdayListResponse>
 }
