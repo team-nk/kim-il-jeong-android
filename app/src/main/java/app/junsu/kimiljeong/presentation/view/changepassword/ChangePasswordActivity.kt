c17602dd2fed9cd6b76bd08e8a8790398bb41740
JunJaBoy <krpjs0508@gmail.com>
2023-01-07 09:46:12 +0900

    feat :: Change Password 추가

---
+++ app/src/main/java/app/junsu/kimiljeong/presentation/view/changepassword/ChangePasswordActivity.kt
@@ -0,0 +1,12 @@
+package app.junsu.kimiljeong.presentation.view.changepassword
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import app.junsu.kimiljeong.R
+
+class ChangePasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change_password)
+    }
+}
