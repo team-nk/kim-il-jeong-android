253c62cf2dc60f335c83f0838a4e4a0052973a29
JunJaBoy <krpjs0508@gmail.com>
2023-01-07 10:11:36 +0900

    refactor :: SignupRequest -> SignUpRequest

--- app/src/main/java/app/junsu/kimiljeong/data/model/remote/request/SignUpRequest.kt
+++ app/src/main/java/app/junsu/kimiljeong/data/model/remote/request/SignUpRequest.kt
@@ -2,7 +2,7 @@ package app.junsu.kimiljeong.data.model.remote.request
 
 import com.google.gson.annotations.SerializedName
 
-data class SignupRequest(
+data class SignUpRequest(
     @SerializedName("email") val email: String,
     @SerializedName("code") val verificationCode: String,
     @SerializedName("account_id") val accountId: String,
