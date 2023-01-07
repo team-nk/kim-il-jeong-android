90fefc775b5254cdf47e107818e3b5c87032425e
JunJaBoy <krpjs0508@gmail.com>
2023-01-07 10:12:49 +0900

    feat :: LoginRequest 구현

---
+++ app/src/main/java/app/junsu/kimiljeong/data/model/remote/request/LoginRequest.kt
@@ -0,0 +1,8 @@
+package app.junsu.kimiljeong.data.model.remote.request
+
+import com.google.gson.annotations.SerializedName
+
+data class LoginRequest(
+    @SerializedName("email") val email: String,
+    @SerializedName("password") val password: String,
+)
