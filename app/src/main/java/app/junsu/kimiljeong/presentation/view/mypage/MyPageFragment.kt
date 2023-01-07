e10a69d06f7e57b94c50e4d961dff3776049926d
Tmdhoon2 <hks026naver@gmail.com>
2023-01-07 20:58:05 +0900

    feat :: MyPageFragment publishing

---
+++ app/src/main/java/app/junsu/kimiljeong/presentation/view/mypage/MyPageFragment.kt
@@ -0,0 +1,22 @@
+package app.junsu.kimiljeong.presentation.view.mypage
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import app.junsu.kimiljeong.R
+import app.junsu.kimiljeong.databinding.FragmentCalendarBinding
+import app.junsu.kimiljeong.databinding.FragmentMapBinding
+import app.junsu.kimiljeong.databinding.FragmentMypageBinding
+import app.junsu.kimiljeong.presentation.base.view.BaseFragment
+
+class MyPageFragment : BaseFragment<FragmentMypageBinding>(
+    R.layout.fragment_mypage,
+) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
