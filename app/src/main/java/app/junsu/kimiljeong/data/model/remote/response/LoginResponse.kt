08084a5a7f0aca97a0c591b0fc063cb22eb1f753
JunJaBoy <krpjs0508@gmail.com>
2023-01-07 14:27:49 +0900

    feat :: LoginResponse 구현

---
+++ app/src/main/java/app/junsu/kimiljeong/data/model/remote/response/LoginResponse.kt
@@ -0,0 +1,8 @@
+package app.junsu.kimiljeong.data.model.remote.response
+
+import com.google.gson.annotations.SerializedName
+
+data class LoginResponse(
+    @SerializedName("access_token") val accessToken: String,
+    @SerializedName("refresh_token") val refreshToken: String,
+)
