c61bed0d666b04e33ad8633524946cf8e473c2bc
JunJaBoy <krpjs0508@gmail.com>
2023-01-07 10:11:06 +0900

    feat :: SignupRequest 구현

---
+++ app/src/main/java/app/junsu/kimiljeong/data/model/remote/request/SignupRequest.kt
@@ -0,0 +1,11 @@
+package app.junsu.kimiljeong.data.model.remote.request
+
+import com.google.gson.annotations.SerializedName
+
+data class SignupRequest(
+    @SerializedName("email") val email: String,
+    @SerializedName("code") val verificationCode: String,
+    @SerializedName("account_id") val accountId: String,
+    @SerializedName("password") val password: String,
+    @SerializedName("re_password") val repeatPassword: String,
+)
