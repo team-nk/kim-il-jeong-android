88b50e23a1bfc4e24e3f9d79d4ab63d420e95223
JunJaBoy <krpjs0508@gmail.com>
2023-01-08 23:25:58 +0900

    feat :: MainViewModel 구현

--- app/src/main/java/com/gram/kimiljeong/presentation/viewmodel/main/MainViewModel.kt
+++ app/src/main/java/com/gram/kimiljeong/presentation/viewmodel/main/MainViewModel.kt
@@ -1,11 +1,48 @@
 package com.gram.kimiljeong.presentation.viewmodel.main
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.gram.kimiljeong.R
+import com.gram.kimiljeong.data.model.remote.response.SelfInformationResponse
 import com.gram.kimiljeong.data.repository.origin.UserRepository
+import com.gram.kimiljeong.presentation.view.base.viewmodel.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
     private val userRepository: UserRepository,
-) : ViewModel()
+    @ApplicationContext application: Application,
+) : BaseViewModel(application) {
+
+    init {
+        getSelfInformation()
+    }
+
+    private val _userInformation = MutableLiveData<SelfInformationResponse>()
+    val userInformation: LiveData<SelfInformationResponse>
+        get() = _userInformation
+
+    internal fun getSelfInformation() {
+        viewModelScope.launch(IO) {
+            kotlin.runCatching {
+                userRepository.getSelfInformation()
+            }.onSuccess {
+                _userInformation.postValue(
+                    if (it.isSuccessful) it.body() else null
+                )
+            }.onFailure {
+                _snackBarMessage.postValue(
+                    mApplication.getString(
+                        R.string.error_failed_to_connect_to_server,
+                    ),
+                )
+            }
+        }
+    }
+}
