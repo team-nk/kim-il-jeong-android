9f739ce29507bd5610c932078533094e0b848109
JunJaBoy <krpjs0508@gmail.com>
2023-01-08 21:46:41 +0900

    feat :: ChangePasswordViewModel Application context 구현

--- app/src/main/java/com/gram/kimiljeong/presentation/viewmodel/changepassword/ChangePasswordViewModel.kt
+++ app/src/main/java/com/gram/kimiljeong/presentation/viewmodel/changepassword/ChangePasswordViewModel.kt
@@ -3,9 +3,10 @@ package com.gram.kimiljeong.presentation.viewmodel.changepassword
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import com.gram.kimiljeong.data.repository.origin.UserRepository
+import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
 class ChangePasswordViewModel @Inject constructor(
+    @ApplicationContext private val mApplication: Application,
     private val repository: UserRepository,
-    private val mApplication: Application,
 ) : AndroidViewModel(mApplication)
