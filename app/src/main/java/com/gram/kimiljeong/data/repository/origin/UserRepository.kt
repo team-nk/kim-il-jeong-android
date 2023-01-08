705089f9627fb8576eb1f33084ccb763cdd92bd5
JunJaBoy <krpjs0508@gmail.com>
2023-01-08 21:30:01 +0900

    feat :: UserRepository 구현

--- app/src/main/java/com/gram/kimiljeong/data/repository/origin/UserRepository.kt
+++ app/src/main/java/com/gram/kimiljeong/data/repository/origin/UserRepository.kt
@@ -1,5 +1,47 @@
 package com.gram.kimiljeong.data.repository.origin
 
-import com.gram.kimiljeong.data.api.remote.AuthAPI
+import com.gram.kimiljeong.data.model.remote.request.*
+import com.gram.kimiljeong.data.model.remote.response.*
+import retrofit2.Response
 
-interface UserRepository : AuthAPI
+interface UserRepository {
+
+    suspend fun renewToken(
+        refreshToken: String,
+    ): Response<RenewTokenResponse>
+
+    suspend fun sendVerificationMail(
+        email: String,
+    ): Response<Void>
+
+    suspend fun signUp(
+        request: SignUpRequest,
+    ): Response<Void>
+
+    suspend fun getSelfInformation(): Response<SelfInformationResponse>
+
+    suspend fun login(
+        request: LoginRequest,
+    ): Response<LoginResponse>
+
+    suspend fun changePassword(
+        request: ChangePasswordRequest,
+    ): Response<Void>
+
+    suspend fun changeBirthdayRequest(
+        request: ChangeBirthdayRequest,
+    ): Response<Void>
+
+    suspend fun checkVerificationCode(
+        email: String,
+        code: String,
+    ): Response<BooleanResponse>
+
+    suspend fun checkIdDuplication(
+        id: String,
+    ): Response<BooleanResponse>
+
+    suspend fun uploadImage(
+        request: UploadImageRequest,
+    ): Response<UploadImageResponse>
+}
