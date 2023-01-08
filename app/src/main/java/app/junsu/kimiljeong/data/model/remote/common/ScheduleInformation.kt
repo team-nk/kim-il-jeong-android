99a2246344f7c1b19aea36a098e0e79d7f6ca426
JunJaBoy <krpjs0508@gmail.com>
2023-01-08 11:34:16 +0900

    refactor :: ScheduleInformation 프로퍼티를 널 허용으로 변경

--- app/src/main/java/app/junsu/kimiljeong/data/model/remote/common/ScheduleInformation.kt
+++ app/src/main/java/app/junsu/kimiljeong/data/model/remote/common/ScheduleInformation.kt
@@ -5,14 +5,14 @@ import app.junsu.kimiljeong.data.util.toColor
 import com.google.gson.annotations.SerializedName
 
 data class ScheduleInformation(
-    @SerializedName("content") val content: String,
-    @SerializedName("address") val address: String,
-    @SerializedName("latitude") val latitude: Float,
-    @SerializedName("longitude") val longitude: Float,
-    @SerializedName("color") private var _color: String,
-    @SerializedName("start_time") val startsAt: String,
-    @SerializedName("end_time") val endsAt: String,
-    @SerializedName("always") val isAllDay: Boolean,
+    @SerializedName("content") val content: String?,
+    @SerializedName("address") val address: String?,
+    @SerializedName("latitude") val latitude: Float?,
+    @SerializedName("longitude") val longitude: Float?,
+    @SerializedName("color") private var _color: String?,
+    @SerializedName("start_time") val startsAt: String?,
+    @SerializedName("end_time") val endsAt: String?,
+    @SerializedName("always") val isAllDay: Boolean?,
 ) {
-    val color: Color = _color.toColor()
+    val color: Color? = _color?.toColor()
 }
