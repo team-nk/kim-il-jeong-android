a46eb275bda5547a72dd7e4bde15617bf7e8b563
Tmdhoon2 <hks026naver@gmail.com>
2023-01-09 14:11:43 +0900

    feat :: activity 흐름 설정

--- app/src/main/java/com/gram/kimiljeong/presentation/view/login/LoginActivity.kt
+++ app/src/main/java/com/gram/kimiljeong/presentation/view/login/LoginActivity.kt
@@ -1,13 +1,29 @@
 package com.gram.kimiljeong.presentation.view.login
 
+import android.content.Intent
 import com.gram.kimiljeong.R
 import com.gram.kimiljeong.databinding.ActivityLoginBinding
 import com.gram.kimiljeong.presentation.view.base.view.BaseActivity
+import com.gram.kimiljeong.presentation.view.main.MainActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class LoginActivity : BaseActivity<ActivityLoginBinding>(
     R.layout.activity_login,
 ) {
-    override fun initView() {}
+    override fun initView() {
+        initLoginButton()
+    }
+
+    private fun initLoginButton() {
+        binding.btnActivityLoginLogin.setOnClickListener {
+            // TODO server login logic
+            startActivity(
+                Intent(
+                    this,
+                    MainActivity::class.java,
+                ),
+            )
+        }
+    }
 }
