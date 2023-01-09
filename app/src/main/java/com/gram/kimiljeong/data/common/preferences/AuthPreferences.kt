9c2a9c4e2f2e8df99853419cd100b763bb641416
JunJaBoy <krpjs0508@gmail.com>
2023-01-09 13:18:46 +0900

    feat :: AuthPreferences 함수 추가

--- app/src/main/java/com/gram/kimiljeong/data/common/preferences/AuthPreferences.kt
+++ app/src/main/java/com/gram/kimiljeong/data/common/preferences/AuthPreferences.kt
@@ -4,9 +4,18 @@ interface AuthPreferences {
 
     fun isLoggedIn(): Boolean
 
+    fun setLoginStatus(loggedIn: Boolean)
+
     fun fetchAccessToken(): String
     fun fetchRefreshToken(): String
 
+    suspend fun fetchTokens(): Pair<String, String>
+
     suspend fun saveAccessToken(token: String)
     suspend fun saveRefreshToken(token: String)
+
+    suspend fun saveTokens(
+        accessToken: String,
+        refreshToken: String,
+    )
 }
