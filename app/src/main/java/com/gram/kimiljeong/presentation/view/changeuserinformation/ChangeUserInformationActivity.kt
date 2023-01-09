5902584e69944ec8f9dc74db5abf7ad2afa148fb
Tmdhoon2 <hks026naver@gmail.com>
2023-01-09 19:08:37 +0900

    feat :: ChangeUserInformationActivity 추가

---
+++ app/src/main/java/com/gram/kimiljeong/presentation/view/changeuserinformation/ChangeUserInformationActivity.kt
@@ -0,0 +1,11 @@
+package com.gram.kimiljeong.presentation.view.changeuserinformation
+
+import com.gram.kimiljeong.R
+import com.gram.kimiljeong.databinding.ActivityChangeUserInformationBinding
+import com.gram.kimiljeong.presentation.view.base.view.BaseActivity
+
+class ChangeUserInformationActivity : BaseActivity<ActivityChangeUserInformationBinding>(
+    R.layout.activity_change_user_information,
+) {
+    override fun initView() {}
+}
