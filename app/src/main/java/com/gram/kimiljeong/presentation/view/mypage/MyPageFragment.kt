e165299d1c5a6f6d3b6d4f5ecbc0faceae524c3c
Tmdhoon2 <hks026naver@gmail.com>
2023-01-09 19:08:49 +0900

    feat :: intent 로직 추가

--- app/src/main/java/com/gram/kimiljeong/presentation/view/mypage/MyPageFragment.kt
+++ app/src/main/java/com/gram/kimiljeong/presentation/view/mypage/MyPageFragment.kt
@@ -1,9 +1,10 @@
 package com.gram.kimiljeong.presentation.view.mypage
 
-import android.widget.TextView
+import app.junsu.startactivityutil.StartActivityUtil.startActivity
 import com.gram.kimiljeong.R
 import com.gram.kimiljeong.databinding.FragmentMypageBinding
 import com.gram.kimiljeong.presentation.view.base.view.BaseFragment
+import com.gram.kimiljeong.presentation.view.changeuserinformation.ChangeUserInformationActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -11,11 +12,27 @@ class MyPageFragment : BaseFragment<FragmentMypageBinding>(
     R.layout.fragment_mypage,
 ) {
     override fun initView() {
-        binding.includedFragmentMypageHeader.run{
+        initMypageHeader()
+        initEditProfileButton()
+    }
+
+    private fun initMypageHeader(){
+        binding.includedFragmentMypageHeader.run {
             tvIncludeGlobalTitle
                 .text = getString(R.string.welcome)
             tvIncludeGlobalSubTitle
                 .text = getString(R.string.my_page)
+        }
+    }
+
+    private fun initEditProfileButton() {
+        binding.btnFragmentMypageEditProfile.setOnClickListener {
+            requireActivity().run {
+                startActivity(
+                    context = this,
+                    to = ChangeUserInformationActivity::class.java,
+                )
+            }
         }
     }
 }
