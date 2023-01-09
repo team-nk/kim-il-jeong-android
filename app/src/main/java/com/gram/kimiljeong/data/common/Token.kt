f0281b80b81ccd5d4c6ab8d89e4db85ba5afa53b
JunJaBoy <krpjs0508@gmail.com>
2023-01-09 13:18:12 +0900

    refactor :: Tokens 클래스명

--- app/src/main/java/com/gram/kimiljeong/data/common/Token.kt
+++
@@ -1,6 +0,0 @@
-package com.gram.kimiljeong.data.common
-
-object Tokens {
-    lateinit var accessToken: String
-    lateinit var refreshToken: String
-}
---
+++ app/src/main/java/com/gram/kimiljeong/data/common/Tokens.kt
@@ -0,0 +1,6 @@
+package com.gram.kimiljeong.data.common
+
+object Tokens {
+    var refreshToken: String? = null
+    var accessToken: String? = null
+}
