b77e4caf026bfef08c664c22cd8fc976ba58cbcd
JunJaBoy <krpjs0508@gmail.com>
2023-01-09 10:34:41 +0900

    feat :: CommonSharedPreferences 구현

---
+++ app/src/main/java/com/gram/kimiljeong/data/common/CommonSharedPreferences.kt
@@ -0,0 +1,12 @@
+package com.gram.kimiljeong.data.common
+
+object SharedPreferencesName {
+    const val DEFAULT = "default"
+}
+
+object SharedPreferencesKey {
+    const val IS_LOGGED_IN = "isLoggedIn"
+    const val IS_INTRODUCTION_PAGER_SHOWN = "isIntroductionPagerShown"
+    const val ACCESS_TOKEN = "accessToken"
+    const val REFRESH_TOKEN = "refreshToken"
+}
