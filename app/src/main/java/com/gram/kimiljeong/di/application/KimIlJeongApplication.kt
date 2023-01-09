bb2e815c2835ebc7aa17b537f0abf034e85a6aa7
JunJaBoy <krpjs0508@gmail.com>
2023-01-09 13:17:31 +0900

    refactor :: KimIlJeongApplication 수정

--- app/src/main/java/com/gram/kimiljeong/di/application/KimIlJeongApplication.kt
+++ app/src/main/java/com/gram/kimiljeong/di/application/KimIlJeongApplication.kt
@@ -1,7 +1,27 @@
 package com.gram.kimiljeong.di.application
 
 import android.app.Application
+import com.gram.kimiljeong.data.common.Token.accessToken
+import com.gram.kimiljeong.data.common.Token.refreshToken
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @HiltAndroidApp
-class KimIlJeongApplication : Application()
+class KimIlJeongApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        initTokens()
+    }
+
+    private fun initTokens() {
+        /*CoroutineScope(Dispatchers.IO).launch {
+            userRepository.fetchTokens().run {
+                accessToken = first
+                refreshToken = second
+            }
+        }*/
+    }
+}
