3badf718446b87b6e335ee7ef106629c5287e887
JunJaBoy <krpjs0508@gmail.com>
2023-01-10 17:18:47 +0900

    feat :: LoginActivity 로그인 로직 구현

--- app/src/main/java/team/nk/kimiljeong/presentation/view/login/LoginActivity.kt
+++ app/src/main/java/team/nk/kimiljeong/presentation/view/login/LoginActivity.kt
@@ -1,16 +1,24 @@
 package team.nk.kimiljeong.presentation.view.login
 
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.viewModels
 import app.junsu.startactivityutil.StartActivityUtil.startActivity
 import dagger.hilt.android.AndroidEntryPoint
 import team.nk.kimiljeong.R
 import team.nk.kimiljeong.databinding.ActivityLoginBinding
 import team.nk.kimiljeong.presentation.base.view.BaseActivity
+import team.nk.kimiljeong.presentation.view.main.MainActivity
 import team.nk.kimiljeong.presentation.view.register.RegisterActivity
+import team.nk.kimiljeong.presentation.viewmodel.login.LoginViewModel
 
 @AndroidEntryPoint
 class LoginActivity : BaseActivity<ActivityLoginBinding>(
     R.layout.activity_login,
 ) {
+
+    private val viewModel by viewModels<LoginViewModel>()
+
     override fun initView() {
         initLoginButton()
         initRegisterButton()
@@ -25,8 +33,50 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>(
     }
 
     private fun initLoginButton() {
-        binding.btnActivityLoginLogin.setOnClickListener {
+        binding.run {
+            btnActivityLoginLogin.setOnClickListener {
+                login(
+                    email = etActivityLoginEmail.text.toString(),
+                    password = etActivityLoginPassword.text.toString(),
+                )
+            }
+        }
+    }
 
+    private fun login(
+        email: String,
+        password: String,
+    ) {
+        viewModel.login(
+            email = email,
+            password = password,
+        )
+    }
+
+    override fun observeEvent() {
+        super.observeEvent()
+        observeIsLoginSucceed()
+    }
+
+    private fun observeIsLoginSucceed() {
+        viewModel.isLoginSucceed.observe(
+            this@LoginActivity,
+        ) {
+            if (it) {
+                startActivity(
+                    this, MainActivity::class.java,
+                )
+            }
+        }
+    }
+
+    override fun observeSnackBarMessage() {
+        super.observeSnackBarMessage()
+        viewModel.snackBarMessage.observe(
+            this@LoginActivity,
+        ) {
+            // TODO make snackBar library
+            Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
         }
     }
 }
