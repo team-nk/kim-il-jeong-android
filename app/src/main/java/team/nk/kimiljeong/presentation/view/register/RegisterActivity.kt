42c9d2202100fe7b4a57080a7ab056b8c00d8c89
JunJaBoy <krpjs0508@gmail.com>
2023-01-10 11:59:49 +0900

    feat :: RegisterActivity 구현

---
+++ app/src/main/java/team/nk/kimiljeong/presentation/view/register/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package team.nk.kimiljeong.presentation.view.register
+
+import team.nk.kimiljeong.R
+import team.nk.kimiljeong.databinding.ActivityRegisterBinding
+import team.nk.kimiljeong.presentation.base.view.BaseActivity
+
+class RegisterActivity : BaseActivity<ActivityRegisterBinding>(
+    R.layout.activity_register,
+) {
+    override fun initView() {
+
+    }
+}
