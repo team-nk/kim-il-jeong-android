6ba8b0794884011822f3ce925304e7b0c6adab35
Tmdhoon2 <hks026naver@gmail.com>
2023-01-10 17:09:11 +0900

    feat ::ChangeUserInformationActivity 버튼 클릭 함수 구현

--- app/src/main/java/team/nk/kimiljeong/presentation/view/changeuserinformation/ChangeUserInformationActivity.kt
+++ app/src/main/java/team/nk/kimiljeong/presentation/view/changeuserinformation/ChangeUserInformationActivity.kt
@@ -1,19 +1,54 @@
 package team.nk.kimiljeong.presentation.view.changeuserinformation
 
+import app.junsu.startactivityutil.StartActivityUtil.startActivity
 import team.nk.kimiljeong.R
 import team.nk.kimiljeong.databinding.ActivityChangeUserInformationBinding
 import team.nk.kimiljeong.presentation.view.base.view.BaseActivity
+import team.nk.kimiljeong.presentation.view.changepassword.ChangePasswordActivity
 
 class ChangeUserInformationActivity : BaseActivity<ActivityChangeUserInformationBinding>(
     R.layout.activity_change_user_information,
 ) {
     override fun initView() {
+        initChangeImageButton()
+        initMoveChangePasswordActivityButton()
         initCancleButton()
+        initChangeInformationButton()
     }
 
-    private fun initCancleButton(){
+    private fun initChangeImageButton(){
+        binding.tvActivityChangeUserInformationChangeImage.setOnClickListener {
+            // TODO server logic
+        }
+    }
+
+    private fun initMoveChangePasswordActivityButton(){
+        binding.tvActivityChangeUserInformationChangePassword.setOnClickListener {
+            startActivity(
+                context = this,
+                to = ChangePasswordActivity::class.java,
+            )
+        }
+    }
+
+    private fun initCancleButton() {
         binding.btnActivityChangeUserInformationCancel.setOnClickListener {
             finish()
+        }
+    }
+
+    private fun initChangeInformationButton() {
+        binding.run {
+            btnActivityChangeUserInformationChange.setOnClickListener {
+                if (
+                    etActivityChangeUserInformationEmail.text.isNotEmpty() &&
+                    etActivityChangeUserInformationId.text.isNotEmpty()
+                ){
+                    // TODO server logic
+                }else{
+                    // TODO show message logic
+                }
+            }
         }
     }
 }
