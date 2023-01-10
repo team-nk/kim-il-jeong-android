227eb508aaaf6b1e3aefdfd001e1ab5c5d8dd026
JunJaBoy <krpjs0508@gmail.com>
2023-01-10 11:15:23 +0900

    feat :: authPreferences의 context를 @ApplicationContext 주입받도록 수정

--- app/src/main/java/team/nk/kimiljeong/data/common/preferences/AuthPreferencesImpl.kt
+++ app/src/main/java/team/nk/kimiljeong/data/common/preferences/AuthPreferencesImpl.kt
@@ -2,6 +2,7 @@ package team.nk.kimiljeong.data.common.preferences
 
 import android.content.Context
 import android.content.Context.MODE_PRIVATE
+import dagger.hilt.android.qualifiers.ApplicationContext
 import team.nk.kimiljeong.data.common.SharedPreferencesKey.ACCESS_TOKEN
 import team.nk.kimiljeong.data.common.SharedPreferencesKey.IS_LOGGED_IN
 import team.nk.kimiljeong.data.common.SharedPreferencesKey.REFRESH_TOKEN
@@ -9,12 +10,10 @@ import team.nk.kimiljeong.data.common.SharedPreferencesName.DEFAULT
 import javax.inject.Inject
 
 class AuthPreferencesImpl @Inject constructor(
-    private val mContext: Context,
+    @ApplicationContext private val mContext: Context,
 ) : AuthPreferences {
 
-    private val mSharedPreferences by lazy {
-        mContext.getSharedPreferences(DEFAULT, MODE_PRIVATE)
-    }
+    private val mSharedPreferences = mContext.getSharedPreferences(DEFAULT, MODE_PRIVATE)
 
     private val mSharedPreferencesEditor by lazy {
         mSharedPreferences.edit()
