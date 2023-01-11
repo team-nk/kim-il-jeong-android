2e6bfc3b1b653b12a19bf928e3b25fccf70e57e7
junsuPark <krpjs0508@gmail.com>
2023-01-11 23:04:05 +0900

    feat :: MyPageFragment viewModel 변수 추가

--- app/src/main/java/team/nk/kimiljeong/presentation/view/post/PostFragment.kt
+++ app/src/main/java/team/nk/kimiljeong/presentation/view/post/PostFragment.kt
@@ -1,13 +1,18 @@
 package team.nk.kimiljeong.presentation.view.post
 
+import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import team.nk.kimiljeong.R
 import team.nk.kimiljeong.databinding.FragmentPostBinding
 import team.nk.kimiljeong.presentation.base.view.BaseFragment
+import team.nk.kimiljeong.presentation.viewmodel.post.PostViewModel
 
 @AndroidEntryPoint
 class PostFragment : BaseFragment<FragmentPostBinding>(
     R.layout.fragment_post,
 ) {
+
+    private val viewModel by viewModels<PostViewModel>()
+
     override fun initView() {}
 }
