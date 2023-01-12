54484073eb07ed034771ebd6146e676ee2387584
junsuPark <krpjs0508@gmail.com>
2023-01-13 00:36:32 +0900

    feat :: ScheduleRepository 구현

---
+++ app/src/main/java/team/nk/kimiljeong/data/repository/remote/origin/ScheduleRepository.kt
@@ -0,0 +1,27 @@
+package team.nk.kimiljeong.data.repository.remote.origin
+
+import retrofit2.Response
+import team.nk.kimiljeong.data.api.remote.ScheduleAPI
+import team.nk.kimiljeong.data.model.remote.common.ScheduleInformation
+import team.nk.kimiljeong.data.model.remote.request.SpecificLocationOfScheduleListRequest
+import team.nk.kimiljeong.data.model.remote.response.InquireScheduleListResponse
+
+interface ScheduleRepository : ScheduleAPI {
+
+    override suspend fun inquireDateScheduleList(date: String): Response<InquireScheduleListResponse>
+
+    override suspend fun createSchedule(request: ScheduleInformation): Response<Void>
+
+    override suspend fun editSchedule(
+        scheduleId: String,
+        request: ScheduleInformation,
+    ): Response<Void>
+
+    override suspend fun removeSchedule(scheduleId: String): Response<Void>
+
+    override suspend fun inquireSpecificLocationOfScheduleList(request: SpecificLocationOfScheduleListRequest): Response<InquireScheduleListResponse>
+
+    override suspend fun inquireEntireScheduleList(): Response<InquireScheduleListResponse>
+
+    override suspend fun inquireChooseScheduleList(): Response<InquireScheduleListResponse>
+}
