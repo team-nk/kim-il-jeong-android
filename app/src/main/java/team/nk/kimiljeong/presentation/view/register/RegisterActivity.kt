c0eb881f26f027a7022ff56690e9f60932649e3e
Tmdhoon2 <hks026naver@gmail.com>
2023-01-12 20:51:36 +0900

    feat :: RegisterActivity 회원가입 로직 구현

--- app/src/main/java/team/nk/kimiljeong/presentation/view/register/RegisterActivity.kt
+++ app/src/main/java/team/nk/kimiljeong/presentation/view/register/RegisterActivity.kt
@@ -1,13 +1,61 @@
 package team.nk.kimiljeong.presentation.view.register
 
+import androidx.activity.viewModels
+import app.junsu.startactivityutil.StartActivityUtil.startActivity
+import dagger.hilt.android.AndroidEntryPoint
 import team.nk.kimiljeong.R
 import team.nk.kimiljeong.databinding.ActivityRegisterBinding
 import team.nk.kimiljeong.presentation.base.view.BaseActivity
+import team.nk.kimiljeong.presentation.util.ShowSnackBarUtil.showShortSnackBar
+import team.nk.kimiljeong.presentation.view.login.LoginActivity
+import team.nk.kimiljeong.presentation.viewmodel.register.RegisterViewModel
 
+@AndroidEntryPoint
 class RegisterActivity : BaseActivity<ActivityRegisterBinding>(
     R.layout.activity_register,
 ) {
-    override fun initView() {
 
+    private val viewModel by viewModels<RegisterViewModel>()
+
+    override fun initView() {
+        initRegisterButton()
+    }
+
+    override fun observeEvent() {
+        super.observeEvent()
+        observeRegister()
+    }
+
+    private fun initRegisterButton() {
+        binding.btnActivityRegisterNext.setOnClickListener {
+            binding.run {
+                viewModel.register(
+                    email = etActivityRegisterEmail.text.toString(),
+                    verificationCode = etActivityRegisterVerificationCode.text.toString(),
+                    accountId = etActivityRegisterId.text.toString(),
+                    password = etActivityRegisterPassword.text.toString(),
+                    passwordRepeat = etActivityRegisterPasswordRepeat.text.toString(),
+                )
+            }
+        }
+    }
+
+    private fun observeRegister() {
+        viewModel.snackBarMessage.observe(
+            this
+        ) {
+            showShortSnackBar(it)
+        }
+        viewModel.register.observe(
+            this,
+        ) {
+            if (it) {
+                showShortSnackBar(getString(R.string.sign_up_dlg_success_title))
+                startActivity(
+                    context = this,
+                    to = LoginActivity::class.java,
+                )
+            }
+        }
     }
 }
