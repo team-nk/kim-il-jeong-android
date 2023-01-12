c16ce8acc09e19e2b00f6238832a97fb766ba1aa
junsuPark <krpjs0508@gmail.com>
2023-01-13 00:36:43 +0900

    feat :: scheduleRepositoryImpl 구현

---
+++ app/src/main/java/team/nk/kimiljeong/data/repository/remote/implement/ScheduleRepositoryImpl.kt
@@ -0,0 +1,55 @@
+package team.nk.kimiljeong.data.repository.remote.implement
+
+import retrofit2.Response
+import team.nk.kimiljeong.data.api.remote.ScheduleAPI
+import team.nk.kimiljeong.data.model.remote.common.ScheduleInformation
+import team.nk.kimiljeong.data.model.remote.request.SpecificLocationOfScheduleListRequest
+import team.nk.kimiljeong.data.model.remote.response.InquireScheduleListResponse
+import team.nk.kimiljeong.data.repository.remote.origin.ScheduleRepository
+import javax.inject.Inject
+
+class ScheduleRepositoryImpl @Inject constructor(
+    private val scheduleAPI: ScheduleAPI,
+) : ScheduleRepository {
+    override suspend fun inquireDateScheduleList(date: String): Response<InquireScheduleListResponse> {
+        return scheduleAPI.inquireDateScheduleList(
+            date = date,
+        )
+    }
+
+    override suspend fun createSchedule(request: ScheduleInformation): Response<Void> {
+        return scheduleAPI.createSchedule(
+            request = request,
+        )
+    }
+
+    override suspend fun editSchedule(
+        scheduleId: String,
+        request: ScheduleInformation,
+    ): Response<Void> {
+        return scheduleAPI.editSchedule(
+            scheduleId = scheduleId,
+            request = request,
+        )
+    }
+
+    override suspend fun removeSchedule(scheduleId: String): Response<Void> {
+        return scheduleAPI.removeSchedule(
+            scheduleId = scheduleId,
+        )
+    }
+
+    override suspend fun inquireSpecificLocationOfScheduleList(request: SpecificLocationOfScheduleListRequest): Response<InquireScheduleListResponse> {
+        return scheduleAPI.inquireSpecificLocationOfScheduleList(
+            request = request,
+        )
+    }
+
+    override suspend fun inquireEntireScheduleList(): Response<InquireScheduleListResponse> {
+        return scheduleAPI.inquireChooseScheduleList()
+    }
+
+    override suspend fun inquireChooseScheduleList(): Response<InquireScheduleListResponse> {
+        return scheduleAPI.inquireChooseScheduleList()
+    }
+}
