02c4431cc0f4912ac7ac18bae8c77c3f03fa8d24
junsuPark <krpjs0508@gmail.com>
2023-01-13 16:20:41 +0900

    feat :: PostRepository createComment 로직 구현

--- app/src/main/java/team/nk/kimiljeong/data/repository/remote/origin/PostRepository.kt
+++ app/src/main/java/team/nk/kimiljeong/data/repository/remote/origin/PostRepository.kt
@@ -1,26 +1,29 @@
 package team.nk.kimiljeong.data.repository.remote.origin
 
 import retrofit2.Response
+import team.nk.kimiljeong.data.api.remote.PostAPI
+import team.nk.kimiljeong.data.model.remote.request.CreateCommentRequest
 import team.nk.kimiljeong.data.model.remote.request.CreatePostRequest
 import team.nk.kimiljeong.data.model.remote.response.InquireBirthdayListResponse
 import team.nk.kimiljeong.data.model.remote.response.InquireCommentListResponse
 import team.nk.kimiljeong.data.model.remote.response.InquirePostListResponse
 
-interface PostRepository {
+interface PostRepository : PostAPI {
 
-    suspend fun inquirePostList(): Response<InquirePostListResponse>
+    override suspend fun inquirePostList(): Response<InquirePostListResponse>
 
-    suspend fun createPost(
+    override suspend fun createPost(
         request: CreatePostRequest,
     ): Response<Void>
 
-    suspend fun createComment(
+    override suspend fun createComment(
         postId: Int,
+        request: CreateCommentRequest,
     ): Response<Void>
 
-    suspend fun inquireBirthdayList(): Response<InquireBirthdayListResponse>
+    override suspend fun inquireBirthdayList(): Response<InquireBirthdayListResponse>
 
-    suspend fun inquireCommentList(
+    override suspend fun inquireCommentList(
         postId: Int,
     ): Response<InquireCommentListResponse>
 }
