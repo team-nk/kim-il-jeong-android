bfb9bc5a25f6cb1fca222d3c5701a9bfeca73bb5
junsuPark <krpjs0508@gmail.com>
2023-01-13 13:19:35 +0900

    feat :: PostInspectActivity 댓글 개수 조회 로직

--- app/src/main/java/team/nk/kimiljeong/presentation/view/postinspect/PostInspectActivity.kt
+++ app/src/main/java/team/nk/kimiljeong/presentation/view/postinspect/PostInspectActivity.kt
@@ -1,9 +1,14 @@
 package team.nk.kimiljeong.presentation.view.postinspect
 
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import team.nk.kimiljeong.R
+import team.nk.kimiljeong.data.model.remote.common.PostInformation
 import team.nk.kimiljeong.databinding.ActivityPostInspectBinding
 import team.nk.kimiljeong.presentation.base.view.BaseActivity
+import team.nk.kimiljeong.presentation.viewmodel.postinspect.PostInspectViewModel
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -11,8 +16,39 @@ class PostInspectActivity @Inject constructor(
 ) : BaseActivity<ActivityPostInspectBinding>(
     R.layout.activity_post_inspect,
 ) {
-    override fun initView() {
 
+    private val viewModel by viewModels<PostInspectViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        viewModel.setPostId(
+            intent.getIntExtra("postId", 0),
+        )
+    }
+
+    override fun initView() {
+        
+    }
+
+    override fun observeEvent() {
+        super.observeEvent()
+        observeComments()
+        observePostInformation()
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun observeComments() {
+        viewModel.comments.observe(
+            this@PostInspectActivity,
+        ) {
+            with(binding) {
+                tvActivityPostInspectCommentsCount.text = "댓글 ${it.size}개"
+            }
+        }
+    }
+
+    private fun observePostInformation() {
     }
 }
 
