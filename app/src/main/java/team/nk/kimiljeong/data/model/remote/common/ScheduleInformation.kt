596470c18bc9544a5d8608a078756dcb8e96e5ef
junsuPark <krpjs0508@gmail.com>
2023-01-13 09:29:10 +0900

    feat :: ScheduleInformation 아이디 추가

--- app/src/main/java/team/nk/kimiljeong/data/model/remote/common/ScheduleInformation.kt
+++ app/src/main/java/team/nk/kimiljeong/data/model/remote/common/ScheduleInformation.kt
@@ -3,6 +3,7 @@ package team.nk.kimiljeong.data.model.remote.common
 import com.google.gson.annotations.SerializedName
 
 data class ScheduleInformation(
+    @SerializedName("schedule_id") val scheduleId: Int?,
     @SerializedName("content") val content: String?,
     @SerializedName("address") val address: String?,
     @SerializedName("latitude") val latitude: Float?,
@@ -10,5 +11,5 @@ data class ScheduleInformation(
     @SerializedName("color") val color: String?,
     @SerializedName("start_time") val startsAt: String?,
     @SerializedName("end_time") val endsAt: String?,
-    @SerializedName("always") val isAllDay: Boolean?,
+    @SerializedName("is_always") val isAllDay: Boolean?,
 )
