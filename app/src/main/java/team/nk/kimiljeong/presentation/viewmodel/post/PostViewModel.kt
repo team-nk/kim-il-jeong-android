e3619a31337f8768195c0c5e9e063a255a52469d
junsuPark <krpjs0508@gmail.com>
2023-01-13 10:03:58 +0900

    refactor :: 잔코드 정리

--- app/src/main/java/team/nk/kimiljeong/presentation/viewmodel/post/PostViewModel.kt
+++ app/src/main/java/team/nk/kimiljeong/presentation/viewmodel/post/PostViewModel.kt
@@ -1,7 +1,6 @@
 package team.nk.kimiljeong.presentation.viewmodel.post
 
 import android.app.Application
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
@@ -33,17 +32,10 @@ class PostViewModel @Inject constructor(
             kotlin.runCatching {
                 postRepository.inquirePostList()
             }.onSuccess {
-                println("HIHIHIHI ${it.isSuccessful}")
                 if (it.isSuccessful) {
-
-                    // todo remove
-                    Log.e(this.javaClass.simpleName, "inquirePosts: ", )
                     _posts.postValue(it.body()!!.posts)
                     println(it.body()) // TODO remove
                 } else {
-
-                    Log.e(this.javaClass.simpleName, "inquirePosts: ", )
-
                     _snackBarMessage.postValue(
                         mApplication.getString(
                             R.string.error_failed_to_connect_to_server,
