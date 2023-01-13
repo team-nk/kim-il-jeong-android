e98893c3ba950a7293b414af2bc26884884faefc
Tmdhoon2 <hks026naver@gmail.com>
2023-01-13 20:49:00 +0900

    feat :: BaseDialogFragment

---
+++ app/src/main/java/team/nk/kimiljeong/presentation/base/view/BaseDialogFragment.kt
@@ -0,0 +1,48 @@
+package team.nk.kimiljeong.presentation.base.view
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.DialogFragment
+
+abstract class BaseDialogFragment<B : ViewDataBinding>(
+    @LayoutRes private val layoutId: Int,
+) : DialogFragment() {
+
+    protected lateinit var binding: B
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(
+            inflater,
+            layoutId,
+            container,
+            false,
+        )
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = viewLifecycleOwner
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        initView()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding.unbind()
+    }
+
+    abstract fun initView()
+
+}
