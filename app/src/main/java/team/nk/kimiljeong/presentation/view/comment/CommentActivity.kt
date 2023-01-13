d036dce3831a76f394f828e1fc990d102ae344ea
junsuPark <krpjs0508@gmail.com>
2023-01-13 15:13:51 +0900

    feat :: CommentActivity 댓글 조회 로직 구현

--- app/src/main/java/team/nk/kimiljeong/presentation/view/comment/CommentActivity.kt
+++ app/src/main/java/team/nk/kimiljeong/presentation/view/comment/CommentActivity.kt
@@ -1,13 +1,34 @@
 package team.nk.kimiljeong.presentation.view.comment
 
+import androidx.activity.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import team.nk.kimiljeong.R
 import team.nk.kimiljeong.databinding.ActivityCommentBinding
+import team.nk.kimiljeong.presentation.adapter.recyclerviewadapter.CommentAdapter
 import team.nk.kimiljeong.presentation.base.view.BaseActivity
+import team.nk.kimiljeong.presentation.viewmodel.comment.CommentViewModel
 
 @AndroidEntryPoint
 class CommentActivity : BaseActivity<ActivityCommentBinding>(
     R.layout.activity_comment,
 ) {
-    override fun initView() {}
+
+    private val viewModel by viewModels<CommentViewModel>()
+
+    override fun initView() {
+    }
+
+    override fun observeEvent() {
+        initComments()
+    }
+
+    private fun initComments() {
+        viewModel.comments.observe(
+            this@CommentActivity,
+        ) {
+            binding.rvActivityCommentList.run {
+                adapter = CommentAdapter(it)
+            }
+        }
+    }
 }
