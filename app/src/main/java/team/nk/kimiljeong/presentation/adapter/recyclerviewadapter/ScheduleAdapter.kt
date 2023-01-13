6a437ca10b3c2321d6f4b524d6ff162643dc3663
junsuPark <krpjs0508@gmail.com>
2023-01-13 09:45:09 +0900

    feat :: ScheduleAdapter 구현

---
+++ app/src/main/java/team/nk/kimiljeong/presentation/adapter/recyclerviewadapter/ScheduleAdapter.kt
@@ -0,0 +1,38 @@
+package team.nk.kimiljeong.presentation.adapter.recyclerviewadapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import team.nk.kimiljeong.data.model.remote.common.ScheduleInformation
+import team.nk.kimiljeong.databinding.ItemScheduleBinding
+
+class ScheduleAdapter(private val schedules: List<ScheduleInformation>) :
+    RecyclerView.Adapter<ScheduleAdapter.ViewHolder>() {
+
+    inner class ViewHolder(private val binding: ItemScheduleBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        internal fun bind(schedule: ScheduleInformation) {
+            with(binding) {
+                tvItemCalendarTodayScheduleTitle.text = schedule.content
+                tvItemCalendarTodayScheduleDate.text = schedule.startsAt ?: "Something Wrong"
+                // TODO indiactor
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            ItemScheduleBinding.inflate(
+                LayoutInflater.from(parent.context), parent, false,
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(schedules[position])
+    }
+
+    override fun getItemCount(): Int {
+        return schedules.size
+    }
+}
