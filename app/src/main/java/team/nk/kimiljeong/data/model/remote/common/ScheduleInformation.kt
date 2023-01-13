495848dec9de58cec4ca1e3e94ba4f676ce4bb8b
junsuPark <krpjs0508@gmail.com>
2023-01-13 09:16:03 +0900

    feat :: scheduleInformation에서 color 파싱 로직 제거(오류 원인)

--- app/src/main/java/team/nk/kimiljeong/data/model/remote/common/ScheduleInformation.kt
+++ app/src/main/java/team/nk/kimiljeong/data/model/remote/common/ScheduleInformation.kt
@@ -1,7 +1,5 @@
 package team.nk.kimiljeong.data.model.remote.common
 
-import team.nk.kimiljeong.data.common.Color
-import team.nk.kimiljeong.data.extension.toColor
 import com.google.gson.annotations.SerializedName
 
 data class ScheduleInformation(
@@ -9,10 +7,8 @@ data class ScheduleInformation(
     @SerializedName("address") val address: String?,
     @SerializedName("latitude") val latitude: Float?,
     @SerializedName("longitude") val longitude: Float?,
-    @SerializedName("color") private var _color: String?,
+    @SerializedName("color") val color: String?,
     @SerializedName("start_time") val startsAt: String?,
     @SerializedName("end_time") val endsAt: String?,
     @SerializedName("always") val isAllDay: Boolean?,
-) {
-    val color: Color? = _color?.toColor()
-}
+)
