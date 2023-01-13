64297618729d3ee18938e58f5f6061767862d72c
junsuPark <krpjs0508@gmail.com>
2023-01-13 09:21:19 +0900

    feat :: CalendarViewModel 구현

---
+++ app/src/main/java/team/nk/kimiljeong/presentation/viewmodel/calendar/CalendarViewModel.kt
@@ -0,0 +1,48 @@
+package team.nk.kimiljeong.presentation.viewmodel.calendar
+
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.launch
+import team.nk.kimiljeong.R
+import team.nk.kimiljeong.data.model.remote.common.ScheduleInformation
+import team.nk.kimiljeong.data.repository.remote.origin.ScheduleRepository
+import team.nk.kimiljeong.presentation.base.viewmodel.BaseViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class CalendarViewModel @Inject constructor(
+    application: Application,
+    private val scheduleRepository: ScheduleRepository,
+) : BaseViewModel(mApplication = application) {
+
+    init {
+        inquireSchedules()
+    }
+
+    private val _schedules = MutableLiveData<List<ScheduleInformation>>()
+    val schedules: LiveData<List<ScheduleInformation>>
+        get() = _schedules
+
+    internal fun inquireSchedules() {
+        viewModelScope.launch(IO) {
+            kotlin.runCatching {
+                scheduleRepository.inquireEntireScheduleList()
+            }.onSuccess {
+                if (it.isSuccessful) {
+                    _schedules.postValue(it.body()?.schedules)
+                } else {
+                    _snackBarMessage.postValue(
+                        mApplication.getString(
+                            R.string.error_failed_to_connect_to_server,
+                        ),
+                    )
+                }
+            }
+        }
+    }
+}
