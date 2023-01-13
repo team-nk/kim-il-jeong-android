3d97d57e2f3a24cbd36ea6ebee8b415497df9af5
junsuPark <krpjs0508@gmail.com>
2023-01-13 14:39:39 +0900

    feat :: CommentActivity 추가

---
+++ app/src/main/java/team/nk/kimiljeong/presentation/view/comment/CommentActivity.kt
@@ -0,0 +1,13 @@
+package team.nk.kimiljeong.presentation.view.comment
+
+import dagger.hilt.android.AndroidEntryPoint
+import team.nk.kimiljeong.R
+import team.nk.kimiljeong.databinding.ActivityCommentBinding
+import team.nk.kimiljeong.presentation.base.view.BaseActivity
+
+@AndroidEntryPoint
+class CommentActivity : BaseActivity<ActivityCommentBinding>(
+    R.layout.activity_comment,
+) {
+    override fun initView() {}
+}
