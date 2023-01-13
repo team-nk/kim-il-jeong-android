b8f780dcd81d74a4f968b317510cff417e9d0849
junsuPark <krpjs0508@gmail.com>
2023-01-13 21:07:47 +0900

    feat :: PostCreateActivity 구현

--- app/src/main/java/team/nk/kimiljeong/presentation/view/postcreate/PostCreateActivity.kt
+++ app/src/main/java/team/nk/kimiljeong/presentation/view/postcreate/PostCreateActivity.kt
@@ -1,14 +1,20 @@
 package team.nk.kimiljeong.presentation.view.postcreate
 
+import androidx.activity.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import team.nk.kimiljeong.R
 import team.nk.kimiljeong.databinding.ActivityPostCreateBinding
 import team.nk.kimiljeong.presentation.base.view.BaseActivity
+import team.nk.kimiljeong.presentation.view.post.selectedPostId
+import team.nk.kimiljeong.presentation.viewmodel.postcreate.PostCreateViewModel
 
 @AndroidEntryPoint
 class PostCreateActivity : BaseActivity<ActivityPostCreateBinding>(
     R.layout.activity_post_create,
 ) {
+
+    private val viewModel by viewModels<PostCreateViewModel>()
+
     override fun initView() {
         initButtons()
     }
@@ -19,13 +25,30 @@ class PostCreateActivity : BaseActivity<ActivityPostCreateBinding>(
     }
 
     private fun initCreateButton() {
-
+        with(binding) {
+            btnActivityCreateNewPostCreate.setOnClickListener {
+                viewModel.createPost(
+                    selectedPostId!!,
+                    etCreateNewPostTitle.text!!.toString(),
+                    etCreateNewPostContent.text!!.toString(),
+                )
+            }
+        }
     }
 
     private fun initCancelButton() {
         binding.btnActivityCreateNewPostCancel.setOnClickListener {
             setResult(RESULT_OK)
             finish()
+        }
+    }
+
+    override fun observeSnackBarMessage() {
+        super.observeSnackBarMessage()
+        viewModel.snackBarMessage.observe(
+            this@PostCreateActivity,
+        ) {
+            showShortSnackBar(it)
         }
     }
 }
