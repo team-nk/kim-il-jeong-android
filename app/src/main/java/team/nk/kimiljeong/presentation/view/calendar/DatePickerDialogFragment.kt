d924506d17bc4c1659dd1c0a993908b7d2ec4390
Tmdhoon2 <hks026naver@gmail.com>
2023-01-14 21:02:20 +0900

    feat :: DataPickerDialogFragment

---
+++ app/src/main/java/team/nk/kimiljeong/presentation/view/calendar/DatePickerDialogFragment.kt
@@ -0,0 +1,13 @@
+package team.nk.kimiljeong.presentation.view.calendar
+
+import team.nk.kimiljeong.R
+import team.nk.kimiljeong.databinding.DialogDatePickerBinding
+import team.nk.kimiljeong.presentation.base.view.BaseDialogFragment
+
+class DatePickerDialogFragment : BaseDialogFragment<DialogDatePickerBinding>(
+    R.layout.dialog_date_picker,
+) {
+    override fun initView() {
+
+    }
+}
