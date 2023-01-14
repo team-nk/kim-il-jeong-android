b90de91e56205106c169babe92fb2f4710df8dd5
Tmdhoon2 <hks026naver@gmail.com>
2023-01-14 22:03:16 +0900

    feat :: DatePickerDialog

--- app/src/main/java/team/nk/kimiljeong/presentation/view/calendar/DatePickerDialogFragment.kt
+++ app/src/main/java/team/nk/kimiljeong/presentation/view/calendar/DatePickerDialogFragment.kt
@@ -1,5 +1,11 @@
 package team.nk.kimiljeong.presentation.view.calendar
 
+import android.content.Context
+import androidx.appcompat.content.res.AppCompatResources
+import com.prolificinteractive.materialcalendarview.CalendarDay
+import com.prolificinteractive.materialcalendarview.DayViewDecorator
+import com.prolificinteractive.materialcalendarview.DayViewFacade
+import com.prolificinteractive.materialcalendarview.format.ArrayWeekDayFormatter
 import team.nk.kimiljeong.R
 import team.nk.kimiljeong.databinding.DialogDatePickerBinding
 import team.nk.kimiljeong.presentation.base.view.BaseDialogFragment
@@ -7,7 +13,54 @@ import team.nk.kimiljeong.presentation.base.view.BaseDialogFragment
 class DatePickerDialogFragment : BaseDialogFragment<DialogDatePickerBinding>(
     R.layout.dialog_date_picker,
 ) {
-    override fun initView() {
 
+    private val today = CalendarDay.today()
+
+    override fun initView() {
+        initCalendar()
+        initCancelButton()
+    }
+
+    private fun initCalendar() {
+        binding.calendarViewDlgDatePickerMain.run {
+            addDecorator(TodayDecorator(requireActivity()))
+            setWeekDayFormatter(
+                ArrayWeekDayFormatter(
+                    resources.getStringArray(
+                        R.array.clndr_day_of_the_week,
+                    ),
+                ),
+            )
+            setTitleFormatter {
+                return@setTitleFormatter "${it.year}년 ${it.month + 1}월"
+            }
+            setOnDateChangedListener { _, date, _ ->
+                if (date == today) {
+                    removeDecorators()
+                } else {
+                    addDecorator(TodayDecorator(requireActivity()))
+                }
+            }
+        }
+    }
+
+    inner class TodayDecorator(context: Context) : DayViewDecorator {
+
+        private val drawable =
+            AppCompatResources.getDrawable(context, R.drawable.bg_calendar_date_today)
+
+        override fun shouldDecorate(day: CalendarDay): Boolean {
+            return day == today
+        }
+
+        override fun decorate(view: DayViewFacade?) {
+            view?.setSelectionDrawable(drawable!!)
+        }
+    }
+
+    private fun initCancelButton(){
+        binding.btnDialogCreateScheduleCancel.setOnClickListener {
+            dismiss()
+        }
     }
 }
--- app/src/main/java/team/nk/kimiljeong/presentation/view/schedule/AddScheduleBottomSheetDialogFragment.kt
+++ app/src/main/java/team/nk/kimiljeong/presentation/view/schedule/AddScheduleBottomSheetDialogFragment.kt
@@ -3,6 +3,7 @@ package team.nk.kimiljeong.presentation.view.schedule
 import team.nk.kimiljeong.R
 import team.nk.kimiljeong.databinding.DialogCreateScheduleBinding
 import team.nk.kimiljeong.presentation.base.view.BaseBottomSheetDialogFragment
+import team.nk.kimiljeong.presentation.view.calendar.DatePickerDialogFragment
 import team.nk.kimiljeong.presentation.view.map.SearchLocationDialog
 
 class AddScheduleBottomSheetDialogFragment :
@@ -12,11 +13,23 @@ class AddScheduleBottomSheetDialogFragment :
 
     override fun initView() {
         initSearchLocationButton()
+        initSelectDayStartButton()
     }
 
-    private fun initSearchLocationButton(){
+    private fun initSearchLocationButton() {
         binding.tvDialogCreateScheduleSearchLocation.setOnClickListener {
             SearchLocationDialog().also {
+                it.show(
+                    requireActivity().supportFragmentManager,
+                    it.tag,
+                )
+            }
+        }
+    }
+
+    private fun initSelectDayStartButton() {
+        binding.btnDialogCreateScheduleDateStart.setOnClickListener {
+            DatePickerDialogFragment().also {
                 it.show(
                     requireActivity().supportFragmentManager,
                     it.tag,
