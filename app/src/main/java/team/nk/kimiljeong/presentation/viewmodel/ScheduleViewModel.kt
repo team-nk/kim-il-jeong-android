c28cd204bf6dd8f8da2329af7eca15982405e85f
Tmdhoon2 <hks026naver@gmail.com>
2023-01-14 17:09:10 +0900

    feat :: ScheduleViewModel

---
+++ app/src/main/java/team/nk/kimiljeong/presentation/viewmodel/ScheduleViewModel.kt
@@ -0,0 +1,31 @@
+package team.nk.kimiljeong.presentation.viewmodel
+
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import dagger.hilt.android.lifecycle.HiltViewModel
+import team.nk.kimiljeong.data.repository.remote.origin.ScheduleRepository
+import team.nk.kimiljeong.presentation.base.viewmodel.BaseViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ScheduleViewModel @Inject constructor(
+    application: Application,
+    private val scheduleRepository: ScheduleRepository,
+) : BaseViewModel(
+    mApplication = application,
+) {
+
+    private val _address = MutableLiveData<String>()
+    val aaddress: LiveData<String>
+        get() = _address
+
+    internal fun setAddress(
+        address: String,
+    ){
+        Log.d("TEST", "fuck")
+        _address.value = address
+        Log.d("TEST", aaddress.value.toString())
+    }
+}
