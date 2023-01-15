424c99939f790e3c1d5301a29fdc0915ab5d67a6
junsuPark <krpjs0508@gmail.com>
2023-01-15 18:30:41 +0900

    feat :: 기본 프로필 이미지 설정

--- app/src/main/java/team/nk/kimiljeong/presentation/adapter/bindingadapter/BindingAdapter.kt
+++ app/src/main/java/team/nk/kimiljeong/presentation/adapter/bindingadapter/BindingAdapter.kt
@@ -3,10 +3,17 @@ package team.nk.kimiljeong.presentation.adapter.bindingadapter
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import team.nk.kimiljeong.R
 
 @BindingAdapter("image")
 fun ImageView.loadImageFrom(
     url: String?,
 ) {
-    Glide.with(this).load(url).into(this)
+    url?.let {
+        Glide.with(this).load(it).into(this)
+    } ?: run {
+        setImageResource(
+            R.drawable.img_global_no_profile,
+        )
+    }
 }
