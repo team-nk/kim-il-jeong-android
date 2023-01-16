d99222e03d97cf947bd8256cbf5a3726fbc7ebd7
Tmdhoon2 <hks026naver@gmail.com>
2023-01-16 10:23:41 +0900

    feat :: BaseMapFragment 추가

---
+++ app/src/main/java/team/nk/kimiljeong/presentation/base/view/BaseMapFragment.kt
@@ -0,0 +1,155 @@
+package team.nk.kimiljeong.presentation.base.view
+
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.LocationManager
+import android.net.Uri
+import android.os.Bundle
+import android.provider.Settings
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.DialogFragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import team.nk.kimiljeong.BuildConfig
+import java.util.*
+
+
+abstract class BaseMapFragment<B : ViewDataBinding>(
+    @LayoutRes private val layoutId: Int,
+) : DialogFragment(), OnMapReadyCallback {
+
+    protected lateinit var binding: B
+
+    protected val mapFragment = SupportMapFragment.newInstance()
+
+    private lateinit var currentLocation: LatLng
+
+    protected lateinit var address: String
+
+    private val locationManager by lazy {
+        requireActivity().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(
+            inflater,
+            layoutId,
+            container,
+            false,
+        )
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = viewLifecycleOwner
+        initView()
+    }
+
+    abstract fun initView()
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        googleMap.run {
+            mapType = GoogleMap.MAP_TYPE_NORMAL
+            addCustomMarker(
+                googleMap = googleMap,
+                latitude = currentLocation.latitude,
+                longtitude = currentLocation.longitude,
+            )
+            setMinZoomPreference(10F)
+            setMaxZoomPreference(18F)
+            moveCamera(
+                CameraUpdateFactory.newLatLng(currentLocation)
+            )
+            animateCamera(
+                CameraUpdateFactory.zoomTo(500F)
+            )
+            setOnMapClickListener {
+                addCustomMarker(
+                    googleMap = googleMap,
+                    latitude = it.latitude,
+                    longtitude = it.longitude,
+                )
+            }
+            setAddress(
+                latitude = currentLocation.latitude,
+                longtitude = currentLocation.longitude,
+            )
+        }
+    }
+
+    private fun addCustomMarker(
+        googleMap: GoogleMap,
+        latitude: Double,
+        longtitude: Double,
+    ) {
+        setAddress(
+            latitude = latitude,
+            longtitude = longtitude,
+        )
+        googleMap.addMarker(
+            MarkerOptions()
+                .title(address)
+                .position(LatLng(latitude, longtitude))
+        )
+    }
+
+    private fun setAddress(
+        latitude: Double,
+        longtitude: Double,
+    ) {
+        address = Geocoder(
+            requireActivity(),
+            Locale.KOREA,
+        ).getFromLocation(
+            latitude,
+            longtitude,
+            1,
+        )?.first()?.getAddressLine(0).toString()
+    }
+
+    protected fun checkGranted(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            requireActivity(),
+            Manifest.permission.ACCESS_FINE_LOCATION,
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    protected fun setUserLocation() {
+        locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)?.run {
+            currentLocation = LatLng(this.latitude, this.longitude)
+            mapFragment.getMapAsync(this@BaseMapFragment)
+        }
+    }
+
+    protected fun moveToOption() {
+        startActivity(
+            Intent(
+                Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                Uri.parse("package:" + BuildConfig.APPLICATION_ID)
+            )
+        )
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding.unbind()
+    }
+}
--- app/src/main/java/team/nk/kimiljeong/presentation/view/map/SearchLocationDialog.kt
+++ app/src/main/java/team/nk/kimiljeong/presentation/view/map/SearchLocationDialog.kt
@@ -1,48 +1,20 @@
 package team.nk.kimiljeong.presentation.view.map
 
-import android.Manifest
 import android.app.Dialog
-import android.content.Context
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.location.Geocoder
-import android.location.LocationManager
-import android.net.Uri
 import android.os.Bundle
-import android.provider.Settings
 import android.view.View
-import androidx.core.content.ContextCompat
 import androidx.core.os.bundleOf
 import androidx.fragment.app.setFragmentResult
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
-import team.nk.kimiljeong.BuildConfig
 import team.nk.kimiljeong.R
 import team.nk.kimiljeong.databinding.DialogSearchLocationBinding
-import team.nk.kimiljeong.presentation.base.view.BaseBottomSheetDialogFragment
-import java.util.*
+import team.nk.kimiljeong.presentation.base.view.BaseMapFragment
 
-class SearchLocationDialog : BaseBottomSheetDialogFragment<DialogSearchLocationBinding>(
+class SearchLocationDialog : BaseMapFragment<DialogSearchLocationBinding>(
     R.layout.dialog_search_location,
 ), OnMapReadyCallback {
-
-    private val mapFragment by lazy {
-        SupportMapFragment.newInstance()
-    }
-
-    private lateinit var currentLocation: LatLng
-
-    private lateinit var address: String
-
-    private val locationManager: LocationManager by lazy {
-        requireActivity().getSystemService(Context.LOCATION_SERVICE) as LocationManager
-    }
 
     override fun onCreateDialog(
         savedInstanceState: Bundle?,
@@ -91,91 +63,13 @@ class SearchLocationDialog : BaseBottomSheetDialogFragment<DialogSearchLocationB
         }
     }
 
-    private fun checkGranted(): Boolean =
-        ContextCompat.checkSelfPermission(
-            requireActivity(),
-            Manifest.permission.ACCESS_FINE_LOCATION,
-        ) == PackageManager.PERMISSION_GRANTED
-
-    override fun onMapReady(googleMap: GoogleMap) {
-        googleMap.run {
-            mapType = GoogleMap.MAP_TYPE_NORMAL
-            addCustomMarker(
-                googleMap = googleMap,
-                latitude = currentLocation.latitude,
-                longtitude = currentLocation.longitude,
-            )
-            setMinZoomPreference(10F)
-            setMaxZoomPreference(18F)
-            moveCamera(
-                CameraUpdateFactory.newLatLng(currentLocation)
-            )
-            animateCamera(
-                CameraUpdateFactory.zoomTo(500F)
-            )
-            setOnMapClickListener {
-                addCustomMarker(
-                    googleMap = googleMap,
-                    latitude = it.latitude,
-                    longtitude = it.longitude,
-                )
-            }
-            setAddress(
-                latitude = currentLocation.latitude,
-                longtitude = currentLocation.longitude,
-            )
-        }
-    }
-
-    private fun addCustomMarker(
-        googleMap: GoogleMap,
-        latitude: Double,
-        longtitude: Double,
-    ) {
-        setAddress(
-            latitude = latitude,
-            longtitude = longtitude,
-        )
-        googleMap.addMarker(
-            MarkerOptions()
-                .title(address)
-                .position(LatLng(latitude, longtitude))
-        )
-    }
-
-    private fun setAddress(
-        latitude: Double,
-        longtitude: Double,
-    ) {
-        address = Geocoder(
-            requireActivity(),
-            Locale.KOREA,
-        ).getFromLocation(
-            latitude,
-            longtitude,
-            1,
-        )?.first()?.getAddressLine(0).toString()
-    }
-
-    private fun setUserLocation() {
-        locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)?.run {
-            currentLocation = LatLng(this.latitude, this.longitude)
-            mapFragment.getMapAsync(this@SearchLocationDialog)
-        }
-    }
-
     private fun initMapView() {
         childFragmentManager.beginTransaction()
-            .replace(R.id.map_dialog_search_location_map_main, mapFragment, "MapTag").commit()
-    }
-
-    private fun moveToOption() {
-        startActivity(
-            Intent(
-                Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
-                Uri.parse("package:" + BuildConfig.APPLICATION_ID)
-            )
-        )
+            .replace(
+                R.id.map_dialog_search_location_map_main,
+                mapFragment,
+                "MapTag",
+            ).commit()
     }
 
     private fun initSelectButton() {
