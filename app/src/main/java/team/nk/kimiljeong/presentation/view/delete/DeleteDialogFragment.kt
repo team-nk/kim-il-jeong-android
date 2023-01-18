46e5696cb594df5f5a54935c9f25304c06cdb5e4
Tmdhoon2 <hks026naver@gmail.com>
2023-01-18 13:39:30 +0900

    feat :: 일정 삭제 기능 구현

--- app/src/main/java/team/nk/kimiljeong/presentation/view/delete/DeleteDialogFragment.kt
+++ app/src/main/java/team/nk/kimiljeong/presentation/view/delete/DeleteDialogFragment.kt
@@ -1,14 +1,35 @@
 package team.nk.kimiljeong.presentation.view.delete
 
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import dagger.hilt.android.AndroidEntryPoint
 import team.nk.kimiljeong.R
 import team.nk.kimiljeong.databinding.DialogDeleteBinding
 import team.nk.kimiljeong.presentation.base.view.BaseDialogFragment
+import team.nk.kimiljeong.presentation.common.ShowSnackBar
+import team.nk.kimiljeong.presentation.util.ShowSnackBarUtil.showShortSnackBar
+import team.nk.kimiljeong.presentation.viewmodel.ScheduleViewModel
 
+@AndroidEntryPoint
 class DeleteDialogFragment: BaseDialogFragment<DialogDeleteBinding>(
     R.layout.dialog_delete,
-) {
+) , ShowSnackBar{
+
+    private val viewModel by viewModels<ScheduleViewModel>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        observeEvent()
+    }
+
     override fun initView() {
         initCancelButton()
+        initDeleteButton()
+    }
+
+    private fun observeEvent(){
+        deleteEvent()
     }
 
     private fun initCancelButton(){
@@ -16,4 +37,26 @@ class DeleteDialogFragment: BaseDialogFragment<DialogDeleteBinding>(
             dismiss()
         }
     }
+
+    private fun initDeleteButton(){
+        viewModel.removeSchedule(requireArguments().getInt("scheduleId"))
+    }
+
+    private fun deleteEvent() {
+        viewModel.removeSchedule.observe(
+            viewLifecycleOwner,
+        ) {
+            if(it){
+                showShortSnackBar(
+                    text = getString(R.string.success_delete),
+                )
+            }
+        }
+    }
+
+    override fun showShortSnackBar(text: String) {
+        binding.root.showShortSnackBar(text)
+    }
+
+    override fun showLongSnackBar(text: String) {}
 }
